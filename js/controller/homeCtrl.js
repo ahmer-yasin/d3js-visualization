dd0446cdaa6d5000c60502ae5547ecd379aeeb62
ahmer-yasin <ahmeryasin1992@gmail.com>
2015-04-11 00:28:20 +0500

    latest question filter is working on q4 or q5 with sentiment facet, create input search box for search facet

--- js/controller/homeCtrl.js
+++ js/controller/homeCtrl.js
@@ -1,9 +1,15 @@
 /**
  * Created by rw on 3/24/15.
  */
-app.controller('homeCtrl',function($scope, $http){
-    $scope.title = "Office of Information Technology";
-    $scope.question = "Q17. Would you recommend the company?";
+app.controller('homeCtrl',function($scope, $http, dataFactory){
+    if (typeof String.prototype.startsWith != 'function') {
+        // see below for better implementation!
+        String.prototype.startsWith = function (str){
+            return this.indexOf(str) === 0;
+        };
+    }
+    $scope.title = "Devision";
+    $scope.question = "Question";
     $scope.index = 0;
     $scope.dataObj = {
         "author":[],
@@ -13,28 +19,74 @@ app.controller('homeCtrl',function($scope, $http){
     $scope.result ={};
     $scope.index = 0;
     $scope.words;
-    $scope.listData = [];
-    $http.get('http://119.81.106.34/searchblox/servlet/SearchServlet?facet=on&pagesize=3&query=*&facet.field=keywords&f.keywords.size=100&facet.field=section&&f.section.size=5&facet.field=author&&f.author.size=100&xsl=json')
-        .success(function(res){
-            console.log(res);
-            $scope.result = res.results.result;
-            $scope.words =  res.facets[0].int;
-            //$scope.dataObj.author = res.facets[2].int;
-            //$scope.listData = res.facets[1].int;
-            $scope.pieChartFunc(res.facets[1].int,res.facets[1]['@count']);
-            console.log($scope.words);
-            angular.forEach(res.results.result,function(v,k){
-                if(v){
-                    $scope.dataObj.section.push(v.section);
-                    $scope.dataObj.keywords.push(v.keywords);
-                }
-            });
-            $scope.wordCloudObj($scope.words);
+    $scope.listData  = [];
+    $scope.devisions = [];
+    $scope.questions = [];
+    $scope.changeIndex= function(index){
+        $scope.index = index
+    };
+    var Questions = function(){
+        $http.get(url)
+            .success(function(d){console.log(d)})
+            .error(function(e){console.log(e)})
+    };
+    var devisionUrl = 'http://119.81.106.34/searchblox/servlet/SearchServlet?facet=true&col=1&query=*&facet.field=division';
+    var url = ' http://119.81.106.34/searchblox/servlet/SearchServlet?facet=true&col=1&query=*';
+    var Api_domain = 'http://119.81.106.34/searchblox/servlet/SearchServlet?facet=on&pagesize=3&query=*&facet.field=keywords&f.keywords.size=100&facet.field=section&&f.section.size=5&facet.field=author&&f.author.size=100&xsl=json'
+    $http.get(Api_domain)
+        .success(function(data){
+            console.log(data);
         })
         .error(function(err){
-            console.log(err)
+            console.log(err);
         });
+    var conditions = {};
+    var sentiments = {};
+    $scope.getFirstTime = function(){
+        $http.get(url)
+            .success(function(res){
+                console.log(res);
+                $scope.result = res.results.result;
+                angular.forEach($scope.result,function(v,key){
+                    if(v.division){
+                        $scope.devisions.push(v.division);
+                    }
+                    if(angular.isObject(v)){
+                        var obj = {};
+                        angular.forEach(v,function(v,k){
+                            if(k.startsWith('Q')){
+                                console.log(k.indexOf('sentiment'));
+                                if(k.indexOf('sentiment') != -1 && k.indexOf('sentiment') == 3 ){
+                                    sentiments[k] = v;
+                                    conditions[key] = sentiments;
+                                }else{
 
+                                    obj[k] = v;
+                                }
+                            }
+                        });
+                        $scope.questions.push(obj);
+                    }
+                });
+                console.log($scope.questions);
+
+
+//            $scope.words =  res.facets[0].int;
+//            $scope.pieChartFunc(res.facets[1].int,res.facets[1]['@count']);
+//            console.log($scope.words);
+//            angular.forEach(res.results.result,function(v,k){
+//                if(v){
+//                    $scope.dataObj.section.push(v.section);
+//                    $scope.dataObj.keywords.push(v.keywords);
+//                }
+//            });
+//            $scope.wordCloudObj($scope.words);
+            })
+            .error(function(err){
+                console.log(err)
+            });
+    };
+    $scope.getFirstTime();
         console.log($scope.result);
         $scope.config = {
             visible: true, // default: true
@@ -43,7 +95,6 @@ app.controller('homeCtrl',function($scope, $http){
             autorefresh: true, // default: true
             refreshDataOnly: true // default: false
         };
-
         $scope.options = {
             chart: {
                 type: 'pieChart',
@@ -96,7 +147,6 @@ app.controller('homeCtrl',function($scope, $http){
     $scope.onClick = function (points, evt) {
         if(points[0].label){
             console.log(points[0].label);
-            //$scope.getFilter(points[0].label, '&f.keywords.filter=');
             $scope.getFilter(points[0].label, '&f.section.filter=');
         }
         else{
@@ -121,7 +171,65 @@ app.controller('homeCtrl',function($scope, $http){
             .error(function(err){
                 console.log(err)
             });
+    };
+
+    $scope.fetchData = function(filter){
+        $('#loading').show();
+        $http.get("http://119.81.106.34/searchblox/servlet/SearchServlet?facet=true&col=1&query="+filter)
+            .success(function(data){
+                $('#loading').hide();
+                $scope.dataFilter = data;
+                console.log($scope.dataFilter);
+            })
+            .error(function(err){
+                $('#loading').hide();
+                console.log(err);
+            })
+    };
+    var conditions = {};
+
+    $scope.QuestionFilter = function(){
+        var facet = '';
+        if(conditions[$scope.index][this.q +'.sentiment']){
+            facet = conditions[$scope.index][this.q +'.sentiment'];
+        }
+      $http.get('http://119.81.106.34/searchblox/servlet/SearchServlet?facet=true&col=1&query='+this.q+':'+$scope.questions[$scope.index][this.q]+'&facet.field='+this.q+'&facet.field='+facet)
+            .success(function(res){
+                console.log(res);
+                if(res.facets[0].int){
+                    $scope.words =  res.facets[0].int;
+                    $scope.wordCloudObj($scope.words);
+                }
+                if(res.facets[1].int){
+                    $scope.pieChartFunc(res.facets[1].int,res.facets[1]['@count']);
+                }
+                $scope.result = res.results.result;
+                $scope.devisions = [];
+                $scope.questions = [];
+                angular.forEach($scope.result,function(v,key){
+                    if(v.division){
+                        $scope.devisions.push(v.division);
+                    }
+                    if(angular.isObject(v)){
+                        var obj = {};
+                        angular.forEach(v,function(v,k){
+                            if(k.startsWith('Q')){
+                                console.log(k.indexOf('sentiment'));
+                                if(k.indexOf('sentiment') != -1 && k.indexOf('sentiment') == 3 ){
+                                    sentiments[k] = v;
+                                    conditions[key] = sentiments;
+                                }else{
+
+                                    obj[k] = v;
+                                }
+                            }
+                        });
+                        $scope.questions.push(obj);
+                    }
+                });
+                })
+            .error(function(err){
+                console.log(err)
+            });
     }
-
-
 });
